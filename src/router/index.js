a90a20c5ca38ebdb68439cfbdaceeacb4a5a4f56
Haotian Wu <3233990645@qq.com>
2021-12-23 13:20:33 +0800

    重要：通过这两个修改来解决打包后的问题

--- src/router/index.js
+++ src/router/index.js
@@ -73,8 +73,8 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
+  mode: "hash",
+  // base: process.env.BASE_URL,
   routes
 });
 
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,20 @@
 module.exports = {
-    publicPath: "/",
-    // port: 8081,
-    devServer: {
-        port: 8081,
-        host: '0.0.0.0',
-    },
+    publicPath: "./",
+
+    // port: 8078,
+    // devServer: {
+    //     port: 8078,
+    //     host: '0.0.0.0',
+    // },
+    // configureWebpack: {
+    //     externals: {
+    //         // 需要使用外部引入的包名：包名
+    //         vue: "Vue",
+    //         "element-ui": "ELEMENT"
+    //     }
+    // },
+
+
+
+
 };
