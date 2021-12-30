6c3631a72166a58f6a118229a5cd6433b996ebd4
zb2313 <2224105295@qq.com>
2021-12-30 20:24:05 +0800

    Update postApi.js

--- src/api/postApi.js
+++ src/api/postApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const baseurl = "http://121.5.137.205:8081/api/post/"
-
+//const baseurl = "http://localhost:8081/api/post/"
 export const deleteContent = (contentId) => {
     let url = baseurl + "deleteContent/" + contentId
     console.log(url)
