e793fd4ec0a364fee6ce7f4a9c822501b05d2076
Wizard23333 <3233990645@qq.com>
2021-12-30 20:10:01 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -75,7 +75,8 @@ axios.interceptors.request.use(function (config) {
   // Do something before request is sent
   let token = localStorage.getItem("token");
   if (token) {
-    config.headers.authorization = `Bearer ${token}`;    //将token放到请求头发送给服务器
+    // config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = token; //将token放到请求头发送给服务器
     return config;
   }
   else {
