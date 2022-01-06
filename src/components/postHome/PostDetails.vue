09524ddf2c5729b15f3e1724b1104a8fe418adad
zb2313 <2224105295@qq.com>
2022-01-06 18:58:07 +0800

    Update PostDetails.vue

--- src/components/postHome/PostDetails.vue
+++ src/components/postHome/PostDetails.vue
@@ -1,50 +1,13 @@
 <template>
   <div
-    :style="{
+      :style="{
       backgroundImage: 'url(' + bcimg + ')',
       backgroundSize: 'contain'
     }"
   >
-<el-row>
-<el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
-</el-row>
-<el-row :gutter="20" >
-<el-col :span="12" style="margin-left: 250px">
-  <div class="grid-content bg-purple" >
-    <el-card>
-      <div style="text-align: left">
-       <span style="margin-left: 0px;font-size:28px;font-weight: bolder">{{this.postInfo.postTitle}}</span>&nbsp;
-        <span>{{this.postInfo.commentNum}}评论/ {{this.postInfo.likeNum}} 点赞</span>
-        <el-button style="float: right" type="text" @click="follow">关注发帖人</el-button>
-        <el-divider></el-divider>
-      </div>
-
-      <el-card style="background: #e2e2e2">
-<!--          <img :src="this.userImage" height="70" width="70" style="float: left">-->
-          <el-avatar :src="this.userImage" :size="40" fit="cover" style="float: left"></el-avatar>
-          <div style="float: left">
-            <span style="font-size:20px;color: #2d8cf0">{{this.postInfo.userId}} &nbsp;</span>
-            <span style="font-size: 14px">{{this.postInfo.lastEditTime}}</span>
-          </div><br><br><br><br>
-         <p style="text-align: left">
-           {{this.postInfo.text}}
-
-         </p>
-        <ul>
-          <li v-for="dt in imgs" :key="dt">
-            <img :src="dt" height="200" width="200">
-          </li>
-        </ul>
-        <div style="float: bottom">
-          <el-button style="font-size: 16px" type="text" class="el-icon-s-opportunity" @click="likePostI">点赞</el-button>({{this.postInfo.likeNum}})
-          <el-button style="font-size: 16px" type="text" class="el-icon-star-off" @click="collectPostI">收藏</el-button>
-          <el-button style="font-size: 16px" type="text" class="el-icon-warning-outline" @click="reportDialogVisible=true">举报</el-button>({{this.postInfo.reportNum}})
-        </div>
-      </el-card><br>
-
     <el-row>
       <el-col :span="24"
-        ><div class="grid-content bg-purple-dark"></div
+      ><div class="grid-content bg-purple-dark"></div
       ></el-col>
     </el-row>
     <el-row :gutter="20">
@@ -53,37 +16,36 @@
           <el-card>
             <div style="text-align: left">
               <span
-                style="margin-left: 0px;font-size:28px;font-weight: bolder"
-                >{{ this.postInfo.postTitle }}</span
+                  style="margin-left: 0px;font-size:28px;font-weight: bolder"
+              >{{ this.postInfo.postTitle }}</span
               >&nbsp;
               <span
-                >{{ this.postInfo.commentNum }}评论/
+              >{{ this.postInfo.commentNum }}评论/
                 {{ this.postInfo.likeNum }} 点赞/ 500浏览</span
               >
               <el-button style="float: right" type="text" @click="follow"
-                >关注发帖人</el-button
+              >关注发帖人</el-button
               >
               <el-divider></el-divider>
             </div>
-
 
             <el-card style="background: #e2e2e2">
               <!--          <img :src="this.userImage" height="70" width="70" style="float: left">-->
               <div @click="showDetailInfo(0)">
                 <el-avatar
-                  :src="this.userImage"
-                  :size="40"
-                  fit="cover"
-                  style="float: left"
+                    :src="this.userImage"
+                    :size="40"
+                    fit="cover"
+                    style="float: left"
                 ></el-avatar>
               </div>
               <div style="float: left">
                 <span style="font-size:20px;color: #2d8cf0"
-                  >{{ this.postInfo.userId }} &nbsp;</span
+                >{{ this.postInfo.userId }} &nbsp;</span
                 >
                 <span style="font-size: 14px">{{
-                  this.postInfo.lastEditTime
-                }}</span>
+                    this.postInfo.lastEditTime
+                  }}</span>
               </div>
               <br /><br /><br /><br />
               <p style="text-align: left" class="pre-text">
@@ -96,45 +58,45 @@
               </ul>
               <div style="float: bottom">
                 <el-button
-                  style="font-size: 16px"
-                  type="text"
-                  class="el-icon-s-opportunity"
-                  @click="likePostI"
-                  >点赞</el-button
+                    style="font-size: 16px"
+                    type="text"
+                    class="el-icon-s-opportunity"
+                    @click="likePostI"
+                >点赞</el-button
                 >({{ this.postInfo.likeNum }})
                 <el-button
-                  style="font-size: 16px"
-                  type="text"
-                  class="el-icon-tickets"
-                  >评论</el-button
+                    style="font-size: 16px"
+                    type="text"
+                    class="el-icon-tickets"
+                >评论</el-button
                 >({{ this.postInfo.commentNum }})
                 <el-button
-                  style="font-size: 16px"
-                  type="text"
-                  class="el-icon-star-off"
-                  @click="collectPostI"
-                  >收藏</el-button
+                    style="font-size: 16px"
+                    type="text"
+                    class="el-icon-star-off"
+                    @click="collectPostI"
+                >收藏</el-button
                 >
                 <el-button
-                  style="font-size: 16px"
-                  type="text"
-                  class="el-icon-warning-outline"
-                  @click="reportDialogVisible = true"
-                  >举报</el-button
+                    style="font-size: 16px"
+                    type="text"
+                    class="el-icon-warning-outline"
+                    @click="reportDialogVisible = true"
+                >举报</el-button
                 >({{ this.postInfo.reportNum }})
               </div> </el-card
             ><br />
 
             <el-dialog
-              title="举报帖子"
-              :visible="reportDialogVisible"
-              style="width: 1000px"
+                title="举报帖子"
+                :visible="reportDialogVisible"
+                style="width: 1000px"
             >
               <el-button
-                type="text"
-                @click="reportDialogVisible = false"
-                style="float: right"
-                >关闭</el-button
+                  type="text"
+                  @click="reportDialogVisible = false"
+                  style="float: right"
+              >关闭</el-button
               >
               <el-checkbox-group v-model="reportList">
                 <el-checkbox label="1">敏感词汇</el-checkbox>
@@ -144,24 +106,17 @@
                 <el-checkbox label="5">其他</el-checkbox><br />
               </el-checkbox-group>
               <el-input
-                style="width: 300px"
-                placeholder="请输入其他举报理由"
+                  style="width: 300px"
+                  placeholder="请输入其他举报理由"
               ></el-input>
               <el-button @click="reportPostI">提交举报</el-button>
             </el-dialog>
 
-      <el-card style="text-align: left">
-        <span style="font-size: 15px">全部评论</span>
-        <el-divider></el-divider>
-        <div>
-          <li v-for="dt in comments" :key="dt">
-            <el-avatar :src="dt.img"></el-avatar>
-            <span style="color: blue">{{dt.name}}</span><br>
-            {{dt.text}}<br>
-            {{dt.lastEditTime}}<br>
-            <el-button style="font-size: 12px" type="text" class="el-icon-s-opportunity" @click="likePostI">点赞</el-button>
-            <el-divider></el-divider>
-          </li>
+            <!--      <el-card style="text-align: left">-->
+            <!--         <span style="font-size: 15px">高赞评论</span>-->
+            <!--        <el-divider></el-divider>-->
+            <!--      </el-card><br>-->
+
             <el-card style="text-align: left">
               <span style="font-size: 15px">全部评论</span>
               <el-divider></el-divider>
@@ -175,17 +130,17 @@
                   {{ dt.text }}<br />
                   {{ dt.lastEditTime }}<br />
                   <el-button
-                    style="font-size: 12px"
-                    type="text"
-                    class="el-icon-s-opportunity"
-                    @click="likePostI"
-                    >点赞</el-button
+                      style="font-size: 12px"
+                      type="text"
+                      class="el-icon-s-opportunity"
+                      @click="likePostI"
+                  >点赞</el-button
                   >
                   <el-button
-                    style="font-size: 12px"
-                    type="text"
-                    class="el-icon-tickets"
-                    >评论</el-button
+                      style="font-size: 12px"
+                      type="text"
+                      class="el-icon-tickets"
+                  >评论</el-button
                   >
                   <el-divider></el-divider>
                 </li>
@@ -196,20 +151,21 @@
               <span style="font-size: 15px">发表评论</span>
               <el-divider></el-divider>
               <v-comment
-                v-bind:fatherId="postID"
-                v-bind:userId="postInfo.userId"
-                v-bind:fathertype="1"
+                  v-bind:fatherId="postID"
+                  v-bind:userId="postInfo.userId"
+                  v-bind:fathertype="1"
               ></v-comment>
             </el-card>
-      </el-card>
-
+          </el-card>
+        </div>
+      </el-col>
       <el-col :span="7">
         <div class="grid-content bg-purple-light">
           <el-row>
             <el-card style="text-align: left">
               <span>广告招商</span>
               <img
-                src="https://img2.baidu.com/it/u=3349915226,4238223520&fm=26&fmt=auto"
+                  src="https://img2.baidu.com/it/u=3349915226,4238223520&fm=26&fmt=auto"
               />
             </el-card> </el-row
           ><br />
@@ -227,7 +183,8 @@
           </el-row>
         </div>
       </el-col>
-        </div>
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -245,7 +202,6 @@ export default {
   data() {
     return {
       bcimg: require("@/assets/scene.jpg"),
-
       title: "标题",
       commentNum: 60,
       posterName: "XZB",
@@ -254,11 +210,8 @@ export default {
       userImage: "",
       postTime: "",
       postID: "",
-
       reportDialogVisible: false,
-
       reportList: [],
-
       imgs: [],
       commentData: [],
       comments: [],
@@ -307,7 +260,6 @@ export default {
         });
       }
     },
-
     async getData() {
       localStorage.setItem("fatherid", this.postID);
       await getbyPostId(this.postID).then(res => {
@@ -318,7 +270,6 @@ export default {
           // console.log('帖子用户头像',res.data)
         });
       });
-
       getPostImgs(this.postID).then(res => {
         this.imgs = res.data.data;
         console.log("帖子图片", this.imgs);
@@ -328,7 +279,6 @@ export default {
         console.log("评论", res.data);
         this.getCommentImgs();
       });
-
       getRankByDay(5).then(res => {
         this.hotPosts = res.data;
         console.log("热帖", res.data.data);
